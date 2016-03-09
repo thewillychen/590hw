ab251bc7eb7be03391d3af4b3db21fdb8a2d8cd7
Willy Chen <terepic@gmail.com>
2016-03-09 03:33:28 -0500

    in progress gf 257

--- hw2/gf257.py
+++ hw2/gf257.py
@@ -10,3 +10,50 @@ def generate257(k, rows):
 	for row in rows:
 		t = np.vstack((t, full[row]))
 	return t[:k,:k]	
+
+def multiply(a,b):
+	return (a+b)%257
+
+def inverse(a): #Need to do this function
+	return operators.inverse(int(a),inv)
+
+def add(a,b):
+	return (a+b)%257
+
+def gauss256(A):
+	n = len(A)
+
+	for i in range(0, n):# Search for maximum in this column
+		maxEl = abs(A[i][i])
+		maxRow = i
+		for k in range(i+1, n):
+			if abs(A[k][i]) > maxEl:
+				maxEl = abs(A[k][i])
+				maxRow = k
+
+		# Swap maximum row with current row (column by column)
+		for k in range(i, n+1):
+			tmp = A[maxRow][k]
+			A[maxRow][k] = A[i][k]
+			A[i][k] = tmp
+
+		# Make all rows below this one 0 in current column
+		for k in range(i+1, n):
+			c = multiply(A[k][i],inverse(A[i][i]))
+			#c = -A[k][i]/A[i][i]
+			for j in range(i, n+1):
+				if i == j:
+					A[k][j] = 0
+				else:
+					A[k][j] = add(A[k][j], multiply(c, A[i][j]))
+					#A[k][j] += c * A[i][j]
+
+	# Solve equation Ax=b for an upper triangular matrix A
+	x = [0 for i in range(n)]
+	for i in range(n-1, -1, -1):
+		x[i] = multiply(A[i][n], inverse(A[i][i]))
+		# x[i] = A[i][n]/A[i][i]
+		for k in range(i-1, -1, -1):
+			A[k][n] = add(A[k][n], multiply(A[k][i],x[i]))
+			# A[k][n] -= A[k][i] * x[i]
+	return x
