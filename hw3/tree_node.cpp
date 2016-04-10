86f0fabb652524d17fe2fb9f27f2e24f6bd49be6
Willy Chen <terepic@gmail.com>
2016-04-10 19:25:44 -0400

    p1, not tested yet

---
+++ hw3/tree_node.cpp
@@ -0,0 +1,42 @@
+#include "tree_node.hpp"
+#include <iostream>
+using namespace std;
+
+Tree_node* find_child(Tree_node *parent, char c){
+	Tree_node *current = parent->first_child;
+	while(current != NULL){
+		if(current->byte == c){
+			return current;
+		}
+		current = current->sibling;
+	}
+	return NULL;
+}
+
+Tree_node* insert_child(Tree_node *parent, char byte, int label){
+	Tree_node* child = new Tree_node(byte, label);
+	child->parent = parent;
+	child->first_child = NULL;
+	child->sibling = NULL;
+
+	if(parent->first_child == NULL){
+		parent->first_child = child;
+	}
+	else{
+		Tree_node *current = parent->first_child;
+		while(current->sibling != NULL){
+			current=current->sibling;
+		}
+		current->sibling = child;
+	}
+	return child;
+}
+
+void print_path(Tree_node *last_node){
+	if(last_node->parent == NULL){ //base case of root
+		return;	
+	} 
+
+	print_path(last_node->parent);
+	cout << last_node->byte;
+}
---
+++ hw3/tree_node.hpp
@@ -0,0 +1,25 @@
+#ifndef TREE_NODE_HPP
+#define TREE_NODE_HPP
+using namespace std;
+
+class Tree_node{
+public:
+	Tree_node();
+
+	int label;
+	char byte;
+	Tree_node *first_child;
+	Tree_node *sibling;
+	Tree_node *parent;
+
+	Tree_node(char newByte, int newLabel){
+		byte = newByte;
+		label = newLabel;
+	}
+};
+
+Tree_node* find_child(Tree_node *parent, char c);
+Tree_node* insert_child(Tree_node *parent, char byte, int label);
+void print_path(Tree_node *last_node);
+
+#endif
